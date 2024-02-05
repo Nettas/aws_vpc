c013b372b71e8df94b292feccfda4a234ccdfd80
Nettas <nettas@aol.com>
2024-02-05 08:50:53 -0500

    inital commit

---
+++ vpc.tf
@@ -0,0 +1,36 @@
+######## Create vpcbu1 ########
+
+resource "aws_vpc" "bu1" {
+  cidr_block       = "172.16.16.0/22"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "tgw-spoke-bu1"
+  }
+}
+
+
+
+######## public subnet bu1 #######
+resource "aws_subnet" "public-vpc-bu1" {
+    vpc_id =  aws_vpc.bu1.id
+    cidr_block = "172.16.16.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "ca-central-1a"
+    # tags {
+    #     Name = "bu1-pub-subnet"
+    # }
+}
+
+resource "aws_subnet" "private-vpc-bu1" {
+    vpc_id =  aws_vpc.bu1.id
+    cidr_block = "172.16.17.0/24"
+    map_public_ip_on_launch = false
+    availability_zone = "ca-central-1a"
+    # tags {
+    #     Name = "bu1-pub-subnet"
+    # }
+}
+
